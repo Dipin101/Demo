e1152067081d3146ece31e3a27592d87cd9cda2c
Dipin101 <khatridipin10@gmail.com>
2026-06-10 14:16:46 -0400

    fix: minor changes

--- backend/server.ts
+++ backend/server.ts
@@ -14,6 +14,7 @@ app.use(
   cors({
     origin: [
       "https://demo-rho-six-79.vercel.app",
+      "https://demo-git-master-dipin101s-projects.vercel.app",
       "http://localhost:5173",
       "http://localhost:3000",
     ],
